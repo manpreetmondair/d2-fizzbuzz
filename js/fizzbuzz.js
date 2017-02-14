e47c231d9c17d922366ee3426c7474a4f4d899b9
MANPREET <mondair.manpreet@gmail.com>
2017-02-14 15:00:18 -0500

    Final Assignment

---
+++ js/fizzbuzz.js
@@ -0,0 +1,34 @@
+// Variable Declarations
+var avow = require('avow');
+
+// Functional Declarations
+function FizzBuzz(n) {
+     // I used for loop to make the flow woking untill i<=n and also I used condtions(if, else, ifelse) //to check the statement, if number(n) pass the condtion then it will return the output otherwise it //will go the next step and check the next condtion. This procee will repeat itself untill i<=n
+
+    var output= '';
+    for(var i=1; i<=n; i++){
+      if (i %3 === 0 && i %5 === 0){
+           output += 'FizzBuzz';
+      }
+      else if(i %3 === 0){
+           output += 'Fizz';
+      }
+      else if(i %5 === 0){
+           output += 'Buzz';
+      }
+      else {
+           output += '.';
+      } 
+    } 
+    return output;
+}
+
+FizzBuzz(15);
+
+// Tests (do not modify these)
+avow('FizzBuzz 1 should be .', '.' === FizzBuzz(1), '.', FizzBuzz(1));
+avow('FizzBuzz 2 should be ..', '..' === FizzBuzz(2), '..', FizzBuzz(2));
+avow('FizzBuzz 3 should be ..Fizz', '..Fizz' === FizzBuzz(3), '..Fizz', FizzBuzz(3));
+avow('FizzBuzz 5 should be ..Fizz.Buzz', '..Fizz.Buzz' === FizzBuzz(5), '..Fizz.Buzz', FizzBuzz(5));
+avow('FizzBuzz 10 should be ..Fizz.BuzzFizz..FizzBuzz', '..Fizz.BuzzFizz..FizzBuzz' === FizzBuzz(10), '..Fizz.BuzzFizz..FizzBuzz', FizzBuzz(10));
+avow('FizzBuzz 15 should be ..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz', '..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz' === FizzBuzz(15), '..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz', FizzBuzz(15));
---
+++ js/flow-control.js
@@ -0,0 +1,82 @@
+//var todayIs = "Monday";
+
+//if( '2' == 2){
+  //  console.log('Basically the same.');
+//}
+//else{
+ //   console.log('So Different.');
+//}
+
+//if (todayIs == 'Tuesday'){
+  //  console.log('Happy Valentine Day');
+//}
+//else if(todayIs === 'wednesday'){
+  //  console.log('Happy Humpday');
+//}
+//else{
+//  console.log(`Happy ${todayIs}`);
+//}
+
+
+//var todayIsTuesday = true;
+//console.log(typeof todayIsTuesday);
+
+//var counter = 0;
+//Counter++;
+//counter = counter + 2;
+//counter += 2;
+//console.log(counter);
+//if (counter>=2){
+ //   console.log('Correct Answer,');
+//}
+
+//var slicesOfPizza = 12;
+
+//if (slicesOfPizza %3 === 0 || slicesOfPizza % 5 === 0) {
+  //  console.log('There\'s enough for both');
+//}
+//else if(slicesOfPizza % 3 === 0){
+  //  Console.log('Enough for three.');
+//}
+//else{
+ //   console.log('Not going to divide evenly');
+//}
+//var temprature = 50;
+
+
+//switch(temprature){
+    //case 50:
+      // console.log('Getting warmer.');
+     //  break;
+   // case 60:
+          // console.log('Getting closer to Spring');
+         //  break;
+   // case 70:
+       //   console.log('Speing is here');
+       //   break;
+   // default:
+           // console.log('Who Knows. Welcome to Indiana.');  
+       // }
+     
+       //  var favouriteColor = 'red' ;
+     //  var message = favouriteColor === 'red' ? 'My favourite color is red ': 'My favorite color is //not red';
+      // console.log(message);
+   
+   // var pets = ['dog', 'cat','hamster', 'fish', 'bird'];
+
+   // for (var i = 0; i < pets.length; i++){
+    //    console.log(pets[i]);
+   // }  
+    //for (var counter = 0; counter < 10; counter ++){
+       // console.log(counter);
+     // for(var i= 0; i < 20; i++){
+     //  document.getElementById('robots').innerHTML += `<img src="http://robohash.org/indy${i}"/>`
+   //}
+
+
+function addOne(number){
+    return number + 1;
+}
+var result = addOne(100);
+
+console.log(result);
